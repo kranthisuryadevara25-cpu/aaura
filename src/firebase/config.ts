0812db448fe846dd9742a8bf550da9ee8bf2c0b7
kranthi suryadevara <kranthisuryadevara25@gmail.com>
2025-10-05 07:58:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9632556640-bd58d",
+  "appId": "1:435313355929:web:c3266137390bee318f5c08",
+  "apiKey": "AIzaSyCGWOFpBL1devnWqA9SSbBkHBM-Ent8OYM",
+  "authDomain": "studio-9632556640-bd58d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "435313355929"
+};
